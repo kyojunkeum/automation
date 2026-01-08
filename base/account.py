b05764b2052ef9f288cd8c8813ffe812bec4e6dc
kyojunkeum <kyojunkeum@gmail.com>
2026-01-08 10:30:25 +0900

    Update environment variable defaults for accounts

--- base/account.py
+++ base/account.py
@@ -3,7 +3,7 @@ import os
 # ============================
 # DLP 제품 접속 정보
 # ============================
-DLP_ADMIN_ID = os.getenv("DLP_ADMIN_ID", "intsoosan")
+DLP_ADMIN_ID = os.getenv("DLP_ADMIN_ID", "admin")
 DLP_ADMIN_PASSWORD = os.getenv("DLP_ADMIN_PASSWORD", "password")
 
 # ============================
@@ -26,49 +26,49 @@ EMAIL_RECEIVER = os.getenv("EMAIL_RECEIVER", "password")
 # ============================
 # Dooray 접속 정보
 # ============================
-DOORAY_ID = os.getenv("DOORAY_ID", "dlptest1")
+DOORAY_ID = os.getenv("DOORAY_ID", "test1")
 DOORAY_PASSWORD = os.getenv("DOORAY_PASSWORD", "password")
 
 # ============================
 # NATE_MAIL 접속 정보
 # ============================
-NATE_ID = os.getenv("NATE_ID", "soosan_kjkeum")
+NATE_ID = os.getenv("NATE_ID", "kjkeum")
 NATE_PASSWORD = os.getenv("NATE_PASSWORD", "password")
 
 # ============================
 # DAUM_MAIL 접속 정보
 # ============================
-DAUM_ID = os.getenv("DAUM_ID", "soosan_kjkeum@naver.com")
+DAUM_ID = os.getenv("DAUM_ID", "kjkeum@naver.com")
 DAUM_PASSWORD = os.getenv("DAUM_PASSWORD", "password")
 
 # ============================
 # OUTLOOK_MAIL (브라우저) 접속 정보
 # ============================
-OUTLOOK_ID = os.getenv("OUTLOOK_ID", "soosan_kjkeum@naver.com")
+OUTLOOK_ID = os.getenv("OUTLOOK_ID", "kjkeum@naver.com")
 OUTLOOK_PASSWORD = os.getenv("OUTLOOK_PASSWORD", "password")
 
 # ============================
 # NAVERWORKS 접속 정보
 # ============================
-NAVERWORKS_ID = os.getenv("NAVERWORKS_ID", "dlptest1@ewakerdlp.by-works.com")
+NAVERWORKS_ID = os.getenv("NAVERWORKS_ID", "id@a.b.com")
 NAVERWORKS_PASSWORD = os.getenv("NAVERWORKS_PASSWORD", "password")
 
 # ============================
 # NOTION 접속 정보
 # ============================
-NOTION_ID = os.getenv("NOTION_ID", "soosankeum@gmail.com")
+NOTION_ID = os.getenv("NOTION_ID", "kjkeum@gmail.com")
 NOTION_PASSWORD = os.getenv("NOTION_PASSWORD", "password")
 
 # ============================
 # COPILOT 접속 정보
 # ============================
-COPILOT_ID = os.getenv("COPILOT_ID", "soosan_kjkeum@naver.com")
+COPILOT_ID = os.getenv("COPILOT_ID", "kjkeum@naver.com")
 COPILOT_PASSWORD = os.getenv("COPILOT_PASSWORD", "password")
 
 
 # ============================
 # YAHOO_MAIL 접속 정보
 # ============================
-# YAHOO_ID = os.getenv("YAHOO_ID", "soosan_kjkeum")
+# YAHOO_ID = os.getenv("YAHOO_ID", "kjkeum")
 # YAHOO_PASSWORD = os.getenv("YAHOO_PASSWORD", "password")
 
